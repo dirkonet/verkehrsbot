084ef2e1dbac8e65a932365256a7ce08c8136841
Dirk Legler <dirko@dirko.net>
2017-01-21 21:14:45 +0100

    Alles Alte entfernt

--- app.py
+++ app.py
@@ -1,20 +1,3 @@
-from bottle import route, run, template
-import logging
-
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-@route('/hello/<name>')
-def hello(name):
-	logger.info('index called.')
-    return template('<b>Hello {{name}}</b>!', name=name)
-
-def index():
-	return template('<b>Index</b>')
-
 def wsgi_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
