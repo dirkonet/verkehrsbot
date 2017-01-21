53c5de1c7fbc8a306d783cd8108e0ceaeed2ab09
Dirk Legler <dirko@dirko.net>
2017-01-21 20:05:23 +0100

    Logging

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from bottle import route, run, template
+import logging
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 @route('/hello/<name>')
 def index(name):
+	logger.info('index called.')
     return template('<b>Hello {{name}}</b>!', name=name)
 
 #run(host='localhost', port=8080)
 
 if __name__ == '__main__':
-    run(server=srv, host="verkehrsbot.azurewebsites.de", port=443)
+    run(server=srv, host='verkehrsbot.azurewebsites.de', port=80)
