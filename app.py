ab5943a6b0a5a89d4f4d7b44af57ef3db1e25f94
Dirk Legler <dirko@dirko.net>
2017-01-21 21:28:06 +0100

    closer to azure sample

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from bottle import route, run, template
+import bottle
+import os
+import sys
+
+# routes contains the HTTP handlers for our server and must be imported.
+import routes
 
 def wsgi_app(environ, start_response):
     return bottle.default_app()
 
-def index():
-	return template('<b>Index</b>')
-
-
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '80'))
+    except ValueError:
+        PORT = 80
 
-    httpd = make_server('verkehrsbot', 80, wsgi_app)
-    httpd.serve_forever()
+    @bottle.route('/static/<filepath:path>')
+    def server_static(filepath):
+        """Handler for static files, used with the development server.
+        When running under a production server such as IIS or Apache,
+        the server should be configured to serve the static files."""
+        return bottle.static_file(filepath, root=STATIC_ROOT)
+
+    # Starts a local test server.
+bottle.run(server='wsgiref', host=HOST, port=PORT)
