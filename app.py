8dcac9e4a42a4efde80d9a5ac69623dfc7fae7f5
Dirk Legler <dirko@dirko.net>
2017-01-21 22:04:41 +0100

    Sample

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+"""
+This script runs the application using a development server.
+"""
+
 import bottle
 import os
 import sys
@@ -5,18 +9,24 @@ import sys
 # routes contains the HTTP handlers for our server and must be imported.
 import routes
 
-def wsgi_app(environ, start_response):
-    return bottle.default_app()
+if '--debug' in sys.argv[1:] or 'SERVER_DEBUG' in os.environ:
+    # Debug mode will enable more verbose output in the console window.
+    # It must be set at the beginning of the script.
+    bottle.debug(True)
 
+def wsgi_app():
+    """Returns the application to make available through wfastcgi. This is used
+    when the site is published to Microsoft Azure."""
+    return bottle.default_app()
 
 if __name__ == '__main__':
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
-    HOST = os.environ.get('SERVER_HOST', 'verkehrsbot.azurewebsites.de')
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '80'))
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
-        PORT = 80
+        PORT = 5555
 
     @bottle.route('/static/<filepath:path>')
     def server_static(filepath):
@@ -26,4 +36,4 @@ if __name__ == '__main__':
         return bottle.static_file(filepath, root=STATIC_ROOT)
 
     # Starts a local test server.
-	bottle.run(server='wsgiref', host=HOST, port=PORT)
+    bottle.run(server='wsgiref', host=HOST, port=PORT)
