e4b83eb77ba302a9c199c695cd4a87a36acbe704
Dirk Legler <dirko@dirko.net>
2017-01-21 22:59:07 +0100

    remove static

--- app.py
+++ app.py
@@ -21,19 +21,11 @@ def wsgi_app():
 
 if __name__ == '__main__':
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get('SERVER_PORT', '80'))
     except ValueError:
-        PORT = 5555
-
-    @bottle.route('/static/<filepath:path>')
-    def server_static(filepath):
-        """Handler for static files, used with the development server.
-        When running under a production server such as IIS or Apache,
-        the server should be configured to serve the static files."""
-        return bottle.static_file(filepath, root=STATIC_ROOT)
+        PORT = 80
 
     # Starts a local test server.
     bottle.run(server='wsgiref', host=HOST, port=PORT)
