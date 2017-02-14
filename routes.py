c16e6bea46c4f098e5f0bf7ae56385df7266455c
Dirk Legler <dirko@dirko.net>
2017-02-14 16:40:24 +0100

    Bot-Test

--- routes.py
+++ routes.py
@@ -4,6 +4,7 @@ Routes and views for the bottle application.
 
 from bottle import route, request, view, run
 import telegram
+from telegram.ext import Dispatcher, CommandHandler
 
 BOT_TOKEN='311882778:AAGrL6E3zf7wFySOzD5gFGm2HFGIDY_hdK8'
 APP_NAME='verkehrsbot'
@@ -26,10 +27,28 @@ def set_hook():
 @route('/botHook', method='POST')
 def bot_hook():
     bot = telegram.Bot(BOT_TOKEN)
+    dispatcher = Dispatcher(bot, None, workers=0)
     update = telegram.update.Update.de_json(request.json, bot)
-    bot.sendMessage(chat_id=update.message.chat_id, text=get_data(update.message.text, update.message.from_user.username))
+
+    dispatcher.add_handler(CommandHandler('Abfahrten', abfahrten, pass_args=True))
+
+    bot.sendMessage(chat_id=update.message.chat_id, text=reply(update.message.text, update.message.from_user.username))
     return 'OK'
 
 
-def get_data(text, username):
-    return 'Hello {}'.format(username)
+def reply(text, username):
+    return 'Hello {}, I got your message.'.format(username)
+
+
+def abfahrten(bot, update, args):
+    if len(args) < 1:
+        return 'Bitte Haltestelle angeben.'
+
+    hst = args[0]
+
+    if len(args) < 2:
+        minutes = 0
+    else:
+        minutes = args[1]
+
+    return 'Abfahrten für {} in {} Minuten:'.format(hst, minutes)
