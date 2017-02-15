cde4580af565a4cb3028dc88e8843169cc1c8af4
Dirk Legler <dirko@dirko.net>
2017-02-15 23:40:42 +0100

    Haltestellenauswertung

---
+++ cleanstations.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('stations.csv', newline='') as infile, open('allstations.csv', newline='', mode='w') as outfile:
+    csv_reader = csv.DictReader(infile, delimiter=';')
+    csv_writer = csv.writer(outfile, delimiter=';')
+    i = 0
+    for row in csv_reader:
+        csv_writer.writerow([i, float(row['WGS84_X'].replace(',', '.')), float(row['WGS84_Y'].replace(',', '.')), row['Name mit Ort']])
+        i+=1
