c73db68f5310cf6c62e4ba2bfd846be9a9574d43
Dirk Legler <dirko@dirko.net>
2017-02-16 11:30:13 +0100

    Logging entfernt

--- routes.py
+++ routes.py
@@ -10,9 +10,6 @@ import csv
 import geopy.distance
 import os, datetime
 
-import sys
-sys.stderr = open('D:\\home\\LogFiles\\stderr.txt', 'w')
-
 BOT_TOKEN='311882778:AAGrL6E3zf7wFySOzD5gFGm2HFGIDY_hdK8'
 APP_NAME='verkehrsbot'
 
@@ -72,37 +69,16 @@ def abfahrten(bot, update, args):
 
 def nearest_station(bot, update):
     # http://stackoverflow.com/a/28368926
-    try:
-        with open('allstations.csv', newline='') as infile:
-            csv_reader = csv.reader(infile, delimiter=';')
-            stations = [(int(row[0]), float(row[2]), float(row[1]), row[3]) for row in csv_reader]
+    with open('allstations.csv', newline='') as infile:
+        csv_reader = csv.reader(infile, delimiter=';')
+        stations = [(int(row[0]), float(row[2]), float(row[1]), row[3]) for row in csv_reader]
 
-            log('Received location lat {}, lon {}'.format(update.message.location.latitude, update.message.location.longitude))
-            coord = (float(update.message.location.latitude), float(update.message.location.longitude))
-            log('Read {} stations and coord {}'.format(len(stations), coord))
-            pts = [geopy.Point(p[1], p[2], p[0]) for p in stations]
-            log('Read {} points, first is {}'.format(len(pts), pts[0]))
-            sts = [p[3] for p in stations]
-            log('Read {} station names, first is {}'.format(len(sts), sts[0]))
-            onept = geopy.Point(coord[0], coord[1])
-            log('onept: {}'.format(onept))
-            alldist = [(p, geopy.distance.distance(p, onept).m) for p in pts]
-            log('Calculated {} distances'.format(len(alldist)))
-            nearest_point = min(alldist, key=lambda x: (x[1]))[0]
-            log('Nearest point id: {}'.format(sts[int(nearest_point.altitude)]))
-            msg = 'Nächstgelegene Station: {} in {:.0f}m'.format(sts[int(nearest_point.altitude)], min(alldist, key=lambda x: (x[1]))[1])
-            log(msg)
-            bot.sendMessage(chat_id=update.message.chat_id, text=msg)
-    except Exception as e:
-        bot.sendMessage(chat_id=update.message.chat_id, text=str(e))
-
-
-def log(txt):
-    """Logs fatal errors to a log file if WSGI_LOG env var is defined"""
-    log_file = os.environ.get('WSGI_LOG')
-    if log_file:
-        f = open(log_file, 'a+')
-        try:
-            f.write('%s: %s\n' % (datetime.datetime.now(), txt))
-        finally:
-            f.close()
+        coord = (float(update.message.location.latitude), float(update.message.location.longitude))
+        pts = [geopy.Point(p[1], p[2], p[0]) for p in stations]
+        sts = [p[3] for p in stations]
+        onept = geopy.Point(coord[0], coord[1])
+        alldist = [(p, geopy.distance.distance(p, onept).m) for p in pts]
+        nearest_point = min(alldist, key=lambda x: (x[1]))[0]
+        msg = 'Nächstgelegene Station: {} in {:.0f}m'.format(sts[int(nearest_point.altitude)],
+                                                             min(alldist, key=lambda x: (x[1]))[1])
+        bot.sendMessage(chat_id=update.message.chat_id, text=msg)
