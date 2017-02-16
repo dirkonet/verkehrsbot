a73fd411214b5cff2f9cfb8ed0abe8a9c9d7be6a
Dirk Legler <dirko@dirko.net>
2017-02-16 15:25:59 +0100

    Keyboard zeilenweise

--- routes.py
+++ routes.py
@@ -89,9 +89,9 @@ def nearest_stations(bot, update, count=5):
         nearest_sts = [sts[int(n.altitude)] for n in nearest_points]
         msg = 'Nächstgelegene Stationen:\n'
 
-        reply_keyboard = [telegram.KeyboardButton(text='/Abfahrten {}'.format(n)) for n in nearest_sts]
+        reply_keyboard = [[telegram.KeyboardButton(text='/Abfahrten {}'.format(n)) for n in nearest_sts]]
         #    nearest_st, nearest_distance, nearest_point.latitude, nearest_point.longitude)
-        bot.sendMessage(chat_id=update.message.chat_id, text=msg, reply_markup=telegram.ReplyKeyboardMarkup([reply_keyboard]))
+        bot.sendMessage(chat_id=update.message.chat_id, text=msg, reply_markup=telegram.ReplyKeyboardMarkup(reply_keyboard))
 
 def nearest_station(bot, update):
     # http://stackoverflow.com/a/28368926
